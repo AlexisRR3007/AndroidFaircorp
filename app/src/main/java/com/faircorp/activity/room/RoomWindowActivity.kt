75042c2ecf92ea8e50c1056e0dac3ff98e2710d9
Alexis RENIER-ROBIN <alexis.renier-robin@etu.emse.fr>
2021-01-13 10:54:54 +0100

    Fix bugs

---
+++ app/src/main/java/com/faircorp/activity/room/RoomWindowActivity.kt
@@ -0,0 +1,144 @@
+package com.faircorp.activity.window
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Switch
+import android.widget.TextView
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import com.faircorp.R
+import com.faircorp.activity.*
+import com.faircorp.api.ApiServices
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class WindowActivity : BasicActivity() {
+
+    var id = 0L;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_window)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        id = intent.getLongExtra(WINDOW_ID_PARAM,0)
+        val name = intent.getStringExtra(WINDOW_NAME_PARAM)
+        val room = intent.getStringExtra(WINDOW_ROOM_PARAM)
+        val floor = intent.getStringExtra(WINDOW_FLOOR_PARAM)
+        val building = intent.getStringExtra(WINDOW_BUILDING_PARAM)
+        val curtemp = intent.getStringExtra(WINDOW_CURTEMP_PARAM)
+        val tartemp = intent.getStringExtra(WINDOW_TARTEMP_PARAM)
+        val status = intent.getStringExtra(WINDOW_STATUS_PARAM)
+
+        val windowName = findViewById<TextView>(R.id.act_room_txt_room_name)
+        val windowRoom = findViewById<TextView>(R.id.act_window_txt_window_room)
+        val windowFloor = findViewById<TextView>(R.id.act_window_txt_window_floor)
+        val windowBuilding = findViewById<TextView>(R.id.act_window_txt_window_building)
+        val windowCurTemp = findViewById<TextView>(R.id.act_window_txt_window_current_temperature)
+        val windowTarTemp = findViewById<TextView>(R.id.act_window_txt_window_target_temperature)
+        val windowStatus = findViewById<TextView>(R.id.act_window_txt_window_status)
+        val windowStatusSwitch = findViewById<Switch>(R.id.act_window_switch_status)
+        val windowDeleteSwitch = findViewById<Switch>(R.id.act_window_switch_delete)
+        val windowDeleteButton = findViewById<TextView>(R.id.act_window_btn_delete)
+
+        windowName.text = name
+        windowRoom.text = room
+        windowFloor.text = floor
+        windowBuilding.text = building
+        windowCurTemp.text = curtemp
+        windowTarTemp.text = tartemp
+        windowStatus.text = status
+        windowStatusSwitch.isChecked = false
+        if(status=="OPEN") {
+            windowStatusSwitch.isChecked = true
+        }
+        windowDeleteSwitch.isChecked = false
+        windowDeleteButton.isClickable = false
+
+    }
+
+    fun onStatusSwitch(view: View) {
+
+        val windowStatusSwitch = findViewById<Switch>(R.id.act_window_switch_status)
+        val windowStatus = findViewById<TextView>(R.id.act_window_txt_window_status)
+
+        // Function is after the click so isChecked=true, it means the window was closed before
+        if(windowStatusSwitch.isChecked) {
+            windowStatusSwitch.isClickable = false;
+            lifecycleScope.launch(context = Dispatchers.IO) {
+                runCatching { ApiServices().windowsApiService.switchStatusById(id).execute() }
+                    .onFailure {
+                        withContext(context = Dispatchers.Main) {
+                            Toast.makeText(
+                                applicationContext,
+                                "Error on switching the status $it",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    }
+                    .onSuccess {
+                        withContext(context = Dispatchers.Main) {
+                            windowStatus.text = "OPEN"
+                        }
+                    }
+            }
+            windowStatusSwitch.isClickable = true;
+        } else {
+            windowStatusSwitch.isClickable = false;
+            lifecycleScope.launch(context = Dispatchers.IO) {
+                runCatching { ApiServices().windowsApiService.switchStatusById(id).execute() }
+                    .onFailure {
+                        withContext(context = Dispatchers.Main) {
+                            Toast.makeText(
+                                applicationContext,
+                                "Error on switching the status $it",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    }
+                    .onSuccess {
+                        withContext(context = Dispatchers.Main) {
+                            windowStatus.text = "CLOSED"
+                        }
+                    }
+            }
+            windowStatusSwitch.isClickable = true;
+        }
+    }
+
+    fun onDeleteSwitch(view: View) {
+        val windowDeleteSwitch = findViewById<Switch>(R.id.act_window_switch_delete)
+        val windowDeleteButton = findViewById<TextView>(R.id.act_window_btn_delete)
+
+        if(windowDeleteSwitch.isChecked) {
+            windowDeleteButton.isClickable = true
+        } else {
+            windowDeleteButton.isClickable = false
+        }
+    }
+
+    fun onDeleteWindow(view: View) {
+
+        val act = this
+
+            lifecycleScope.launch(context = Dispatchers.IO) {
+                runCatching { ApiServices().windowsApiService.deleteById(id).execute() }
+                    .onFailure {
+                        withContext(context = Dispatchers.Main) {
+                            Toast.makeText(
+                                applicationContext,
+                                "Error on switching the status $it",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    }
+                    .onSuccess {
+                        withContext(context = Dispatchers.Main) {
+                            act.finish()
+                        }
+                    }
+
+        }
+    }
+}
---
+++ app/src/main/java/com/faircorp/activity/room/RoomWindowsActivity.kt
@@ -0,0 +1,90 @@
+package com.faircorp.activity.window
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.faircorp.R
+import com.faircorp.activity.*
+import com.faircorp.api.ApiServices
+import com.faircorp.api.window.WindowDto
+import com.faircorp.model.window.OnWindowSelectedListener
+import com.faircorp.model.window.WindowsAdapterView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class WindowsActivity : BasicActivity(), OnWindowSelectedListener {
+
+    var adapter : WindowsAdapterView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_windows)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.act_windows_list)
+        adapter = WindowsAdapterView(this)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = adapter
+
+        lifecycleScope.launch(context = Dispatchers.IO) {
+            runCatching { ApiServices().windowsApiService.findAll().execute() }
+                .onSuccess {
+                    withContext(context = Dispatchers.Main) {
+                        adapter?.update(it.body() ?: emptyList())
+                    }
+                }
+                .onFailure {
+                    withContext(context = Dispatchers.Main) {
+                        Toast.makeText(
+                            applicationContext,
+                            "Error on windows loading $it",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        lifecycleScope.launch(context = Dispatchers.IO) {
+            runCatching { ApiServices().windowsApiService.findAll().execute() }
+                .onSuccess {
+                    withContext(context = Dispatchers.Main) {
+                        adapter?.update(it.body() ?: emptyList())
+                    }
+                }
+                .onFailure {
+                    withContext(context = Dispatchers.Main) {
+                        Toast.makeText(
+                            applicationContext,
+                            "Error on windows loading $it",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+        }
+    }
+
+    override fun onWindowSelected(window : WindowDto) {
+        val intent = Intent(this, WindowActivity::class.java)
+            .putExtra(WINDOW_ID_PARAM, window.id)
+            .putExtra(WINDOW_NAME_PARAM, window.name)
+            .putExtra(WINDOW_ROOM_PARAM, window.room.name)
+            .putExtra(WINDOW_FLOOR_PARAM, window.room.floor.floorNumber.toString())
+            .putExtra(WINDOW_BUILDING_PARAM, window.room.floor.building.name)
+            .putExtra(WINDOW_CURTEMP_PARAM, window.room.currentTemperature?.toString())
+            .putExtra(WINDOW_TARTEMP_PARAM, window.room.targetTemperature?.toString())
+            .putExtra(WINDOW_STATUS_PARAM, window.windowStatus.toString())
+
+        startActivity(intent)
+    }
+}
